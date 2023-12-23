d74762af33d0fd35bafc9dfa52e814887bc87e7c
biswarupz <biswarupsen.work@gmail.com>
2023-12-23 22:44:02 +0530

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const about = document.querySelector(".aboutBtn")
 const pop = document.querySelector(".popDiv")
 const banner =  document.querySelector(".banner")
+const navbar = document.getElementById("navbar")
+const contact = document.querySelector(".contactBtn")
+
 about.addEventListener("click",function(){
     pop.style.display = "block"
     banner.style.display = "none"
@@ -10,4 +13,25 @@ about.addEventListener("click",function(){
 
 
     },10000)
+})
+
+
+var rect = document.querySelector("#rect");
+
+
+window.addEventListener("mousemove", function (details) {
+  console.log(details.clientX);
+  console.log(details.clientY);
+  gsap.to("#rect", {
+    left: details.clientX + "px",
+    top: details.clientY + "px",
+    ease: Power4,
+  });
+});
+
+about.addEventListener("mousemove",function(){
+    rect.style.display="none"
+})
+contact.addEventListener("mousemove",function(){
+    rect.style.display="none"
 })
